8dd000dbc0a6ecd15499e0b289a86edb094c0187
vloveits <hathuong.1990@outlook.com>
2021-06-06 01:09:27 +0900

    add componet

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Rating from './Rating';
+
+export default function Product(props) {
+    const { product } = props;
+    return (
+        <div key={product._id} className="card">
+            <a href={`/product/${product._id}`}>
+                {/* <!-- image size: 680px by 830px --> */}
+                <img className="medium" src={product.image} alt={product.name} />
+            </a>
+            <div className="card-body">
+                <a href={`/product/${product._id}`}>ß
+                    <h2>{product.name}</h2>
+                </a>
+                <Rating
+                    rating={product.rating}
+                    numReviews={product.numReviews}
+                ></Rating>
+                <div className="price">${product.price}</div>
+            </div>
+        </div>
+    )
+}
