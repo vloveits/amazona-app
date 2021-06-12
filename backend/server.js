b94aa4869244da22022a73902509632989f6f343
vloveits <hathuong.1990@outlook.com>
2021-06-12 11:58:00 +0900

    update

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-app.listen(5000, () => {
-  console.log("Server at http://localhost:5000");
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server at http://localhost:${port}`);
 });
