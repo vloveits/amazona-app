541a5b90fab7161db77526dcce5a2a241078da69
vloveits <hathuong.1990@outlook.com>
2021-06-22 00:02:35 +0900

    update

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 // export const addToCart = (productId, qty) => async (dispatch, getState) => {
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
@@ -8,12 +8,17 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
     type: CART_ADD_ITEM,
     payload: {
       name: data.name,
-      image: data.name,
+      image: data.image,
       price: data.price,
       countInStock: data.countInStock,
       product: data._id,
       qty,
     },
   });
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (productId) => (dispatch, getState) => {
+  dispatch({ type: CART_REMOVE_ITEM, payload: productId });
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
