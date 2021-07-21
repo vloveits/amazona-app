593b304e4311a073e3a7f2220e8541fd09bd6210
vloveits <hathuong.1990@outlook.com>
2021-07-22 00:55:15 +0900

    add order

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,26 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import productRouter from "./routers/productRouter.js";
-import userRouter from "./routers/userRouter.js";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import productRouter from './routers/productRouter.js';
+import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect(process.env.MONDODB_URL || "mongodb://localhost/amazona", {
+mongoose.connect(process.env.MONDODB_URL || 'mongodb://localhost/amazona', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
 
-app.use("/api/users", userRouter);
-app.use("/api/products", productRouter);
-app.get("/", (req, res) => {
-  res.send("Server is ready");
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
+app.get('/', (req, res) => {
+  res.send('Server is ready');
 });
 
 app.use((err, req, res, next) => {
--- backend/utils.js
+++ backend/utils.js
@@ -13,3 +13,24 @@ export const generateToken = (user) => {
     }
   );
 };
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.header.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXX
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || 'somthingsecret',
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: 'Invalid Token' });
+        } else {
+          req.user = decode;
+          next();
+        }
+      }
+    );
+  } else {
+    res.status(401).send({ message: 'No Token' });
+  }
+};
