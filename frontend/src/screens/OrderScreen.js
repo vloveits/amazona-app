a54bede445bec7a9f98e7611c85e52e6bb95b3c7
vloveits <hathuong.1990@outlook.com>
2021-08-01 11:36:34 +0900

    xoa import

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import CheckoutSteps from '../components/CheckoutSteps';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import LoadingBox from '../components/LoadingBox';
